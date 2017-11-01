bd9e54ac321ae1b2079bfe10e15418e2b58c1c8a
Lawrence <lawrencejcoles@gmail.com>
2017-11-01 17:45:28 +0000

    #10 JAX-RS and Jersey set up for the business servlet

---
+++ src/main/java/servlets/RestConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package servlets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+/**
+ * @author Lawrence
+ *
+ */
+@ApplicationPath("/*")
+public class RestConfig extends Application {
+
+    public RestConfig() {
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> returnValue = new HashSet<Class<?>>();
+        returnValue.add(BusinessServlet.class);
+        return returnValue;
+    }
+}
