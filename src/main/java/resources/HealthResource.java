598636b129be7f12a03d3ce3bf91cd0bc347a4bb
DomHoney <ddhoney97@hotmail.co.uk>
2017-11-16 23:53:32 +0000

    Change health resource to root uri, remove readiness probe
    this is quicker than stopping and restarting ingress resource

--- src/main/java/resources/HealthResource.java
+++ src/main/java/resources/HealthResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/health")
+@Path("/")
 public class HealthResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
