b62110605376ae3589d1c8484472c49738336809
DomHoney <ddhoney97@hotmail.co.uk>
2017-12-10 15:14:03 +0000

    #31-client-db-query-fix: Implement ClientResource

--- src/main/java/webresources/ClientResource.java
+++ src/main/java/webresources/ClientResource.java
@@ -1,63 +1,91 @@
 /**
- * 
+ *
  */
 package webresources;
 
-import models.Client;
-
-import javax.ws.rs.*;
+import database.BadKeyException;
+import database.DBClientQueries;
+import database.NoDataStoreConnectionException;
+import java.util.ArrayList;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
+import models.Client;
 
 /**
  * Client servlet to handle client processing.
- *  
+ *
  * @author Lawrence
  */
-@Path("/business/{buisnessTag}/client")
-public class ClientResource
-{
-	/**
-	 * Getter for getting a client by its id.
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("{clientId}")
-	public Client getWorkLog(@PathParam("buisnessTag") String businessTag,
-			@PathParam("clientId") int clientId)
-	{	
-		Client client = new Client();
-		
-		return client;
-	}
-	
-	/**
-	 * Returns all businesses if no tag is given.
-	 * 
-	 * @return the requested business.
-	 */
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<Client> getClients() 
-	{	
-		ArrayList<Client> result = new ArrayList<Client>();
-		
-		Client client = new Client(10, "Frank", "baesystems");
-		result.add(client);
-		
-		return result;
-	}
-	
-	/**
-	 * Post method for creating a new client.
-	 */
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response putWorkLog(@PathParam("buisnessTag") String businessTag, 
-			Client client)
-	{
-		return Response.status(200).entity("").build();
-	}
+@Path("/business/{businessTag}/client")
+public class ClientResource {
+
+    /**
+     * Getter for getting a client by its id.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{clientId}")
+    public Client getClient(@PathParam("clientId") int clientId) {
+        try {
+            Client result = new DBClientQueries().getClient(clientId);
+
+            if (result != null) {
+                return result;
+            } else {
+                throw new WebApplicationException(Response.Status.NOT_FOUND);
+            }
+        } catch (NoDataStoreConnectionException e) {
+            throw new WebApplicationException(Response.Status.BAD_GATEWAY);
+        }
+    }
+
+    /**
+     * Returns all businesses if no tag is given.
+     *
+     * @return the requested business.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<Client> getClients(@PathParam("businessTag") final String businessTag) {
+        ArrayList<Client> result = new ArrayList<Client>();
+
+        try {
+            result = new DBClientQueries().getAllClients(businessTag);
+        } catch (NoDataStoreConnectionException e) {
+            throw new WebApplicationException(Response.Status.BAD_GATEWAY);
+        }
+
+        return result;
+    }
+
+    /**
+     * Post method for creating a new client.
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putClient(@PathParam("businessTag") String businessTag,
+        Client client) {
+        try {
+            // manually set businessTag if not present in model
+            if (client.getBusinessTag().isEmpty()) {
+                client.setBusinessTag(businessTag);
+            }
+
+            new DBClientQueries().createClient(client);
+
+            return Response.status(200).entity("").build();
+        } catch (BadKeyException e) {
+            return Response.status(400).entity("Invalid businessTag").build();
+        } catch (NoDataStoreConnectionException e) {
+            throw new WebApplicationException(Response.Status.BAD_GATEWAY);
+        }
+    }
 }
 
